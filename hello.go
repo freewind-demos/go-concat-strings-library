7c30df89b5b6da5a478cef3d84d9b31537580198
freewind <nowindlee@gmail.com>
2018-08-05 18:46:37 +0800

    demo

--- hello.go
+++ hello.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"bytes"
+)
+
+func concatStrs(strs ...string) string {
+	var buffer bytes.Buffer
+	for i := 0; i < len(strs); i++ {
+		buffer.WriteString(strs[i])
+	}
+	return buffer.String()
+}
 
 func main() {
-	fmt.Println("Hello go!")
+	var finalStr = concatStrs("Hello, ", "Go!")
+	fmt.Println(finalStr)
 }
